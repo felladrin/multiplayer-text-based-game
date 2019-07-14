53b10d72638b59a13b2a01caa16a7c85ac1d39d8
Victor Nogueira <felladrin@gmail.com>
2019-07-14 12:32:42 +0300

    Load commands dynamically
    So now they work as plugins.

--- src/server/commands/index.ts
+++
@@ -1,6 +0,0 @@
-import "./get";
-import "./getFromSomewhere";
-import "./say";
-import "./sleep";
-import "./smile";
-import "./smileToSomeone";
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,15 @@
 import * as express from 'express';
 import {ServerToClientEvent} from "../common/enum/ServerToClientEvent";
-import './commands';
 import {io} from "./instances/io";
 import {http} from "./instances/http";
 import {join} from "path";
 import {app} from "./instances/app";
 import {eventToActionMap} from "./instances/eventToActionMap";
+
+require('require-all')({
+    dirname     :  __dirname.concat('/commands'),
+    filter      :  new RegExp(`(.*)\\.${__filename.split('.').pop()}$`)
+});
 
 io.on('connection', socket => {
     io.emit(ServerToClientEvent.AppendToEventsPanel, `${socket.id} has joined!`);
@@ -22,5 +26,5 @@ app.use(express.static(join(__dirname, '..', '..', 'dist')));
 const port = process.env.PORT || 3000;
 
 http.listen(port, () => {
-    console.log('listening on *:' + port);
+    console.log(`Server started on port ${port}`);
 });
