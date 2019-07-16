c64d3f93eb864eb48fcf359c9b60872223d5d35f
Victor Nogueira <felladrin@gmail.com>
2019-07-16 23:53:34 +0300

    Replace 'ts-node' by `tsc` + `node .` for when running the server in production mode

--- src/server/instances/app.ts
+++ src/server/instances/app.ts
@@ -2,4 +2,4 @@ import express = require("express");
 
 export const app = express();
 
-app.use(express.static(__dirname.concat("/../../../dist")));
+app.use(express.static(__dirname.concat("/../../../build/client")));
