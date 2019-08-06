b1ee63649904ccc3fed2d550725a9256379ce1d0
Victor Nogueira <felladrin@gmail.com>
2019-08-06 21:46:17 +0300

    Print out the public IP when game server starts

--- src/server/instances/webServer.ts
+++ src/server/instances/webServer.ts
@@ -1,10 +1,21 @@
 import { createServer } from "http";
 import { expressApp } from "./expressApp";
+import http from "http";
 
 export const webServer = createServer(expressApp);
 
 const port = process.env.PORT || 3000;
 
 webServer.listen(port, (): void => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Game is now locally available at http://localhost:${port}`);
+
+  http.get({ hostname: "api.ipify.org" }, (response): void => {
+    let publicIp = "";
+    response.on("data", (chunk): void => {
+      publicIp += chunk;
+    });
+    response.on("end", (): void => {
+      console.log(`And publicly available at http://${publicIp}:${port}`);
+    });
+  });
 });
